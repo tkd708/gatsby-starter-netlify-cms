f2e0b0f98d119bf8b2f210c5973288c23bf833cf
tkd708 <take708gym@gmail.com>
2019-12-27 00:07:32 +1000

    styles

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -43,6 +43,7 @@ export const IndexPageTemplate = ({
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
             backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
+            alignItems: 'center',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -56,6 +57,7 @@ export const IndexPageTemplate = ({
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
             backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
+            alignItems: 'center',
             lineHeight: '1',
             padding: '0.25em',
           }}
