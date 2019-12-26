bdf9ba73d96c28e9accd1495d7efb265eababdb8
tkd708 <take708gym@gmail.com>
2019-12-27 01:28:21 +1000

    api

--- lambda/api/hello.js
+++ lambda/api/hello.js
@@ -5,7 +5,8 @@ module.exports.handler = async function(event, context) {
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
-      msg: "Hello, World! This is better " + Math.round(Math.random() * 10)
+      msg: "Hello, World! This is better " + Math.round(Math.random() * 10),
+      txt: "sample text"
     })
   }
 }
