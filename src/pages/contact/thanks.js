4d3c7b0d88f1adca6e088e3105dee3c541360fe4
tkd708 <take708gym@gmail.com>
2020-06-03 19:57:27 +1000

    contact form

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from 'react';
+import Layout from '../../components/Layout';
 
 export default () => (
-  <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
-      </div>
-    </section>
-  </Layout>
-)
+    <Layout>
+        <section className="section">
+            <div className="container">
+                <div className="content">
+                    <h1>Thank you!</h1>
+                    <p>I will get it back to you soon.</p>
+                </div>
+            </div>
+        </section>
+    </Layout>
+);
